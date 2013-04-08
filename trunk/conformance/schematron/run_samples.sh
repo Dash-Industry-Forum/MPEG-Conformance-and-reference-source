856ceb9fdcf9bf85a8141d7ba02cf4b187cced60
mwaltl <mwaltl@1f8cbe5a-c36e-48dc-8afe-c845dd49f816>
2013-04-08 08:19:14 +0000

    Removed examples/results not needed anymore due to removed rulesets 13 and 14.

--- trunk/conformance/schematron/run_samples.sh
+++ trunk/conformance/schematron/run_samples.sh
@@ -44,10 +44,3 @@ java -jar ../saxon9.jar -versionmsg:off -s:examples/ex43.mpd -o:output/result_ex
 java -jar ../saxon9.jar -versionmsg:off -s:examples/ex44.mpd -o:output/result_ex44.xml -xsl:output/val_schema.xsl
 java -jar ../saxon9.jar -versionmsg:off -s:examples/ex45.mpd -o:output/result_ex45.xml -xsl:output/val_schema.xsl
 java -jar ../saxon9.jar -versionmsg:off -s:examples/ex46.mpd -o:output/result_ex46.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex47.mpd -o:output/result_ex47.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex48.mpd -o:output/result_ex48.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex49.mpd -o:output/result_ex49.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex50.mpd -o:output/result_ex50.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex51.mpd -o:output/result_ex51.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex52.mpd -o:output/result_ex52.xml -xsl:output/val_schema.xsl
-java -jar ../saxon9.jar -versionmsg:off -s:examples/ex53.mpd -o:output/result_ex53.xml -xsl:output/val_schema.xsl
